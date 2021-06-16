a1de82bd9b449b3ba4ef7864061865d11048b004
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-06-16 14:53:53 +0900

    userのプロフィール画像を登録できるようにしました

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
-  process resize_to_limit: [500, 500]
+  # process resize_to_fit: [500, 500]
+  process resize_to_fill: [500, 500, "Center"]
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
