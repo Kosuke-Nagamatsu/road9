9eaaa008dde1e52718c211a5edf8f03ea5450831
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-06-19 18:54:55 +0900

    デザインを修正しました

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,10 +4,10 @@ class FavoritesController < ApplicationController
   end
   def create
     favorite = current_user.favorites.create(feed_id: params[:feed_id])
-    redirect_to feeds_path, notice: "#{favorite.feed.user.name}さんのブログをお気に入り登録しました"
+    redirect_to feeds_path, notice: "#{favorite.feed.user.name}さんの写真をお気に入り登録しました！"
   end
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to feeds_path, notice: "#{favorite.feed.user.name}さんのブログをお気に入り解除しました"
+    redirect_to feeds_path, notice: "#{favorite.feed.user.name}さんの写真をお気に入り解除しました！"
   end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,7 +2,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
   # process resize_to_fit: [500, 500]
-  process resize_to_fill: [500, 500, "Center"]
+  process resize_to_fill: [500, 600, "Center"]
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
